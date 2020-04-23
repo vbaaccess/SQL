a0c98a6f1846354ec7209f580dccad55023fe540
koter <pablodd@wp.pl>
2020-04-23 10:32:39 +0200

    Update SQL - ID search in the whole database.sql

--- SQL - ID search in the whole database.sql
+++ SQL - ID search in the whole database.sql
@@ -1,6 +1,16 @@
 DECLARE @SoughtIdField VARCHAR(254)
 DECLARE @SoughtId VARCHAR(254)
 
+SET @SoughtId = ''		-- variable initialization
+SET @SoughtIdField = ''	-- variable initialization
+
+SET @SoughtIdField='IdPracownika' --- search field, e.g.: IdType,Id
+SET @SoughtId = 'IS NOT NULL'     --- search phrase in the search field: IN (123) , IS NULL, >100
+
+DECLARE @cur CURSOR
+DECLARE @cur_SQLToTest NVARCHAR(MAX)
+DECLARE @cur_SQLToShow NVARCHAR(MAX)
+DECLARE @rccount INT
 DECLARE @SearchStringCount INT
 
 DECLARE @lp INT
